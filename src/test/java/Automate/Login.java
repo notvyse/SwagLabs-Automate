07a2c06fa431deed3b84755a90776536432ac54a
vyse <amrtyamadhb@gmail.com>
2023-09-29 22:23:56 +0530

    Automate

---
+++ src/test/java/Automate/Login.java
@@ -0,0 +1,25 @@
+package Automate;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login {
+	public static String browser = "Chrome";
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+		if(browser.equals("Chrome")){
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("problem_user");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+	}
+		
+}
---
+++ src/test/java/Automate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author notvy
+ *
+ */
+package Automate;
